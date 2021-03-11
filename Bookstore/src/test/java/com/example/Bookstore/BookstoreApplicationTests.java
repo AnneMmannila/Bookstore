58ca64b863d1a3be9d1d33d66b7cd0736a16cdc6
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-03-11 14:35:37 +0200

    test added

--- Bookstore/src/test/java/com/example/Bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/com/example/Bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.Bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.example.Bookstore.web.BookstoreController;
+import org.junit.runner.RunWith;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookstoreController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
